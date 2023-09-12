39a98bc539a9a1b44f7c373b756a85897aefece8
Charline-L <charlinelaportefernandez@gmail.com>
2023-09-12 14:49:37 +0200

    Ajout de l'exercice 2

---
+++ TD1/exercices/exercice-2.js
@@ -0,0 +1,45 @@
+/* EXERCICE 2 - 20 minutes
+
+
+Documentation utile : 
+- If/Else : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Statements/if...else#exemples
+- For : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Statements/for 
+- Opérateurs de comparaison : https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Expressions_and_operators#op%C3%A9rateurs_de_comparaison
+
+
+#1 Créer une boucle qui :
+- affiche les nombres de 0 à 10
+- si le nombre est égale à 0 doit en plus afficher "Début"
+- si le nombre est égale à 5 doit en plus afficher "Moitié"
+- si le nombre est égale à 10 doit en plus afficher "Fin"
+
+==> Résultat dans la console : 
+
+Début
+1
+2
+3
+4
+Moitié
+6
+7
+8
+9
+FIN
+
+<===
+
+
+
+#2 Créer une boucle qui à partir du tableau ci-après: 
+- affiche toutes les lettres de l'alphabet dans une même String
+
+===> Résultat dans la console : 
+
+abcdefghijklmnopqrstuvwxyz
+
+<=== 
+
+*/
+
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
