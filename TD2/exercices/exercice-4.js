caeb234dd5d1d2bcf51634c110aeffc803eb312f
Charline-L <charlinelaportefernandez@gmail.com>
2023-09-13 14:35:33 +0200

    Ajout du code de l'après-midi et de l'exercice 4

---
+++ TD2/exercices/exercice-4.js
@@ -0,0 +1,5 @@
+/* EXERCICE 4 - 15 minutes
+
+Créer 3 boutons qui changent la couleur de ma page lorsque je clique dessus.
+
+*/
---
+++ TD2/mercredi-aprem/evenements.js
@@ -0,0 +1,30 @@
+/* 
+LES EVENEMENTS EN JAVASCRIPT 
+
+click
+hover
+scroll
+taper des touches
+rester appuyer
+touch
+.
+.
+.
+ma video est terminée
+ma video commence sa lecture... 
+*/
+
+// Pour écouter un évènement j'ai besoin de plusieurs choses : 
+// 1 : qui (j'écoute) : quel élément HTML ?
+// 2 : quoi : quel évènement j'écoute ? click, hover, press...
+// 3 : ce qui doit se faire si fait l'action
+const button = document.querySelector('button')
+/* 
+Comment écrire un add event listener : 
+qui.addEventListener('quoi', function(){
+    le code que je dois faire
+})
+*/ 
+button.addEventListener('click', function() {
+    console.log('click')
+})
