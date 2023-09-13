1bfb9f88af5bed59e10f4c6ec9a61c9d04573447
Charline-L <charlinelaportefernandez@gmail.com>
2023-09-13 12:18:47 +0200

    Ajout de l'exercice et du cours du matin

---
+++ TD1/exercices/exercice-3.js
@@ -0,0 +1,23 @@
+/* EXERCICE 3 - 30 minutes
+
+À partir d'une balise "a" vide dans votre HTML
+Générez un hyperlien qui redirige vers la page linkedin de notre utilisateur.
+Pour cela vous devez demander le nom d'utilisateur du visiteur.
+Si le nom d'utilisateur est vide ajouter la class disabled en CSS qui rend le lien inactif.
+Si le nom d'utilisateur est rempli le lien doit s'ouvrir dans un nouvel onglet au click
+
+Documentation utile : 
+- Prompt : https://developer.mozilla.org/fr/docs/Web/API/Window/prompt#exemples
+- Contenu : https://developer.mozilla.org/fr/docs/Web/API/Node/textContent
+- Attribut : https://developer.mozilla.org/fr/docs/Web/API/Element/setAttribute
+- Class : https://developer.mozilla.org/fr/docs/Web/API/Element/classList
+- Curseurs en CSS : https://developer.mozilla.org/fr/docs/Web/CSS/cursor
+
+===> Exemples de résultat attendu
+
+Avec un nom d'utilisateur : 
+<a href="https://www.linkedin.com/in/charline-laporte/">Voir mon profil Linkedin</a>
+
+Sans nom d'utilisateur : 
+<a href="" class="disabled">Je n'ai pas de profil Linkedin</a>
+*/
---
+++ TD1/mercredi-matin/le-dom.js
@@ -0,0 +1,30 @@
+/* 
+MANIPULATION DE DOM 
+DOM = Document Object Model
+*/
+
+// document = "copie" de notre HTML mais version Javascript
+
+// Modifier en ajoutant du HTML à un élément existant
+const myDiv = document.querySelector(".my-div")
+console.log(myDiv)
+myDiv.innerHTML = '<ul><li>Changer le contenu de ma div</li></ul>'
+
+// Modifier le texte de l'élément
+const para = document.querySelector("#para")
+para.textContent = 'Je change juste le texte et pas le HTML </br>'
+
+// Modifier le style CSS
+const paraRed = document.querySelector('.text')
+// background-color: red
+paraRed.style.backgroundColor = '#00FF00'
+
+// Ajouter ou supprimer des classes CSS
+const button = document.querySelector('button')
+button.classList.add('disabled')
+button.classList.remove('blue')
+
+// Modifier les attributs
+const img = document.querySelector("img")
+// setAttribute(duNom , deLaValeur)
+img.setAttribute("src", "img/Wiki.jpg")
