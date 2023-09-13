eb335614c71b381860040a28c06b3f6b192714cd
Charline-L <charlinelaportefernandez@gmail.com>
2023-09-13 15:23:59 +0200

    Ajout cours après-mid

---
+++ TD2/exercices/exercice-5.js
@@ -0,0 +1,7 @@
+/* EXERCICE 5 - 10 minutes
+
+Créer un programme qui affiche ce que je tape dans une div
+- si j'appuye sur la touche espace on change de couleur la typo
+- couleurs possibles pink, purple, yellow
+
+*/
--- TD2/mercredi-aprem/evenements.js
+++ TD2/mercredi-aprem/evenements.js
@@ -14,6 +14,8 @@ ma video est terminée
 ma video commence sa lecture... 
 */
 
+
+// LE CLICK
 // Pour écouter un évènement j'ai besoin de plusieurs choses : 
 // 1 : qui (j'écoute) : quel élément HTML ?
 // 2 : quoi : quel évènement j'écoute ? click, hover, press...
@@ -27,4 +29,11 @@ qui.addEventListener('quoi', function(){
 */ 
 button.addEventListener('click', function() {
     console.log('click')
+})
+
+
+
+// LE CLAVIER
+window.addEventListener('keypress', function(event) {
+    console.log('touche appuyée', event.key)
 })
